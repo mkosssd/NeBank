638bd06930c7ef502c6381fbb79c53aae3958a81
Manas Khandelwal <manas@slt.work>
2024-08-12 12:28:45 +0530

    code update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,25 +1,29 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSideBar from '@/components/RightSideBar'
 import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
-  const loggedIn = { firstName: 'Manas' }
+  const loggedIn = { firstName: 'Manas', lastName: 'Khandelwal', email: 'mkosssd@gmail.com' }
   return (
     <section className='home'>
       <div className='home-content'>
-        <div className='home-header'>
+        <header className='home-header'>
           <HeaderBox
             title='Welcome'
             type='greeting'
             user={loggedIn.firstName || 'Guest'}
-			subtext='Access and manage your account and transactions efficiently.'
+            subtext='Access and manage your account and transactions efficiently.'
           />
           <TotalBalanceBox
-          accounts={[]}
-          totalBanks={1}
-          totalCurrentBalance={1390.95}/>
-        </div>
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={5750.95}
+          />
+        </header>
+        RECENT TRANSACTIONS
       </div>
+      <RightSideBar user={loggedIn} transactions={[]} banks={[{},{}]}/>
     </section>
   )
 }
---
+++ components/BankCard.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+import React from 'react'
+
+const BankCard = ({showBalance, account, userName}: CreditCardProps) => {
+  return (
+    <div className='flex flex-col'>
+        <Link href='/' className='bank-card'>
+            
+        </Link>
+    </div>
+  )
+}
+
+export default BankCard
